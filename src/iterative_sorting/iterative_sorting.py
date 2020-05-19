932209315ef56f800109c2311641fcac50e44b89
jonalynn masters <jonalynna.masters@gmail.com>
2020-05-18 18:58:50 -0700

    selection sort and bubble sort completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,31 +1,61 @@
+import random
+array = list(range(1, 21))
+random.shuffle(array)
+
 # TO-DO: Complete the selection_sort() function below
+
+
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+    for i in range(0, len(arr) - 1):        # selection_sort takes in an array, i is the index starting at the 0 index and running through the length of the array -1 which would be the last indexed item
+        print(arr)
+        cur_index = i                       # the current index we are on is represented with i
+        smallest_index = cur_index          # Looking for the smallest index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        # Your code here
+        # for each item (j) in the array with i range of index + 1 through length of the array
+        for j in range(i + 1, len(arr)):
+            # if the smallest index of the array is > array j:
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j  # The smallest index of the array goes to j
 
-
-        # TO-DO: swap
-        # Your code here
-
+# TO-DO: swap
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
+array = list(range(1, 21))
+random.shuffle(array)
+print(array)
+print(selection_sort(array))
+
+
 def bubble_sort(arr):
-    # Your code here
-
-
+    for i in range(0, len(arr) - 1):
+        print(arr)  # Outer loop: i = 0 to num of items in list minus 1
+        # Inner loop: i = 0 to num of items in list minus 1 minus i because we know from the outer loop the last item in the list is already sorted
+        for j in range(0, len(arr) - 1 - i):
+            # if item on the left is bigger than item on the right:
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]  # swap places
     return arr
+
+
+array_2 = list(range(1, 21))
+random.shuffle(array_2)
+print(array_2)
+print(bubble_sort(array_2))
+
+# NOTE: This works too, not sure why we don't just do this
+array_3 = list(range(1, 11))
+random.shuffle(array_3)
+print(array_3)
+array_3.sort()
+print(array_3)
 
 
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
-
 
     return arr
